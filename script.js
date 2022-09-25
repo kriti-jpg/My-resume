8ccbfdd21ed1265bc58e8e04dacf6ce060d5c396
Kriti <kriti.sthaaa@gmail.com>
2022-09-25 22:39:30 +0545

    myresume

---
+++ script.js
@@ -0,0 +1,11 @@
+var number1 = 9;
+var number2 = 8;
+var finalresult = number1 * number2;
+
+
+document.getElementById("javascriptbtn").addEventListener("hover", displaymyname);
+
+function displaymyname(){
+      alert("the addition of two number result is" + finalresult)
+}
+
