e0658041b1af0f65c036980f587391d7e7377785
Kriti <kriti.sthaaa@gmail.com>
2022-10-08 22:25:50 +0545

    learned about hoe to add click event on button in js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-var numberone = 10;
-var numbertwo = 20;
-var result = numberone * numbertwo;
-console.log(result);
+var clickyourbutton = document.getElementById('Mybutton');
+console.log(clickyourbutton)
+clickyourbutton.addEventListener('click',function(e) {
+      alert('Click your button');
+});
+
+
+var enterbutton = document.getElementById('Pressme');
+console.log(enterbutton)
+enterbutton.addEventListener('click',function(e) { 
+alert('Press the button!!!')
+});
+
+var entryme = document.getElementById('Enterbutton');
+console.log(entryme)
+entryme.addEventListener('click',function(e){
+      alert('Enter your name please');
+});
