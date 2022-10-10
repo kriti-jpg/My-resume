22f8968fc68a849508e1a1ac73f73462064f60d4
Kriti <kriti.sthaaa@gmail.com>
2022-10-10 22:08:16 +0545

    Learned about adding,subract,multiplying with variables

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-var isWeekDay = "Friday";
+var numberone = 1;
+var numbertwo = 2;
+var sum = numberone + numbertwo;
+var numone = 8;
+var numtwo = 4;
+var subtract = numone - numtwo;
 
-if(isWeekDay == "Monday"){
-      console.log("Today is Monday")
-      
-}
-else if(isWeekDay == "Tuesday"){
-      console.log("Today is Tuesday")
-}
 
-else if(isWeekDay == "Wednesday"){
-      console.log("Today is Wednesday")
-}
-else if(isWeekDay == "Thursday"){
-      console.log("Today is Thursday")
-}
+var one = 9;
+var two = 9;
+var multiply = one * two;
+console.log(multiply)
 
-else if(isWeekDay == "Friday"){
-      console.log("Today is Friday")
-
-}
-else if(isWeekDay == "Saturday"){
-      console.log("Today is Saturday")
-}
-
-else if(isWeekDay == "Sunday"){
-      console.log("Today is Sunday")
-}
