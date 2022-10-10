9d2571e65fb5dc672a6460a71f6d5b38ed40121b
Kriti <kriti.sthaaa@gmail.com>
2022-10-10 22:38:01 +0545

    Learned about template litrals backtick

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function Addtwonum(number1,number2){
-console.log(number1 * number2)
+console.log(`if we multiplied number1 and number2 we will get result as ${number1 * number2 } 
+
+`)
 }
-Addtwonum(9,1)
+Addtwonum(9,2)
